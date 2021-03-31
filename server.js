d2b3ef76ceceba1d79810db91588e5b11187aa2c
lwod <lwodcurrentmail@gmail.com>
2021-03-31 13:51:42 +0300

    add socketio and socket-event-listener to the server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const socketio = require('socket.io')
+
 const http = require('http')
 
 const path = require('path')
@@ -9,8 +11,14 @@ const PORT = 3000 || process.env.PORT
 
 const server = http.createServer(app)
 
+const io = socketio(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+
+io.on('connection', socket=>{
+	console.log('new ws connection...')
+})
 
 server.listen(PORT, ()=>{
 	console.log('server run: ', PORT)
